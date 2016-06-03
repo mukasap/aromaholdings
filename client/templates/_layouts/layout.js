ce63bad69ed7be9b9ab365aa4c72bdfd937c6c63
developer <mukasap@gmail.com>
2016-06-03 15:38:23 +0300

    Theming done, create read me and add config

--- client/templates/_layouts/layout.js
+++ client/templates/_layouts/layout.js
@@ -1,12 +1,12 @@
 //master
 Template.master.onRendered(function(){
-	$('body').addClass('hold-transition skin-red layout-top-nav');
+	$('body').addClass('hold-transition skin-blue layout-top-nav');
 	$('body').removeClass('fixed sidebar-mini');
 
 });
 
 //dashboard
 Template.dashboard.onRendered(function(){
-	$('body').addClass('hold-transition skin-red fixed sidebar-mini');
+	$('body').addClass('hold-transition skin-blue fixed sidebar-mini');
 	$('body').removeClass('layout-top-nav');
 });
